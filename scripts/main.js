bb8117b984601b1151bc98c40268909fbe970381
Andrew Le <andrewle19@csu.fullerton.edu>
2018-03-06 01:52:43 -0800

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+(function (window){
+  'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var myTruck = new Truck('Falcon',new DataStore());
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
+  window.myTruck = myTruck;
+})(window);
